249caafab603ce39fa1f70a7ffa6022e5971df40
Hamza <mac@MacBook-Pro-de-Mac.local>
2020-03-25 22:23:07 +0100

    in

--- src/uis/Editor.js
+++ src/uis/Editor.js
@@ -3,9 +3,38 @@ import './Editor.css'
 
 const Editor = () => {
     const [focus, setFocus] = useState(false)
-    const [content, setContent] = useState([""])
+    const [content, setContent] = useState(["¨"])
     const [line, setLine] = useState(0)
 
+    const handleKeyDown = (e) => {
+        switch (e.key) {
+            case "ArrowLeft":
+
+                
+                let arr = [...content]
+                const index = arr[line].indexOf("¨")
+
+                let a = arr[line].split("")
+                let x = a[index-1]
+                a[index-1] = a[index]  
+                a[index] = x
+                arr[line] = a.join("")
+                
+                setContent(arr)
+
+                
+
+                break;
+
+            case "ArrowRight":
+
+                break;
+            default:
+                break;
+        }
+
+        
+    }
     const handleKeyPress = (e) => {
         switch (e.key) {
             case "Enter":
@@ -15,34 +44,43 @@ const Editor = () => {
 
             default:
                 let arr = [...content]
-                arr[line] = arr[line].concat(e.key)
+                arr[line] = arr[line].concat(e.key).replace("¨", "").concat("¨")
+
                 setContent(arr)
                 break;
         }
-
-        e.stopPropagation()
         e.preventDefault()
+
+
     }
+
     return (
+
         <div className={`editor`}
             onMouseDown={() => setFocus(true)}
             tabIndex="0"
             onKeyPress={handleKeyPress}
+            onKeyDown={handleKeyDown}
         >
             <pre>
                 {
+
                     content.map((l, index) =>
                         (<div key={`e-${index}`}>
-                            {(l.split("").length == 0) ?
-                                <br />
-                                :
-                                l.split("").map((char, i) =>
-                                    (<span key={i}>{char}</span>)
-                                )}
+                            {
+                                (l.split("").length === 0) ?
+                                    <br />
+                                    :
+                                    l.split("").map((char, i) =>
+                                        (char === "¨") ?
+                                            focus && <span key={i} className="cursor blink"></span>
+                                            :
+                                            (<span key={i}>{char}</span>)
+                                    )}
                         </div>)
                     )
                 }
-                {focus && <span className="cursor blink"></span>}
+
             </pre>
         </div>
     )
